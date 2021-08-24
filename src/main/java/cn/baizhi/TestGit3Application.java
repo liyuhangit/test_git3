fab31b3bda015cd72520eee76268916e0e4c77c5
lyh <597488721.qq.com>
2021-08-24 15:13:34 +0800

    第3次提交的应学项目，v1.0

---
+++ src/main/java/cn/baizhi/TestGit3Application.java
@@ -0,0 +1,13 @@
+package cn.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGit3Application.class, args);
+    }
+
+}
